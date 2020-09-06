6229391b364dff90b4b94720ef7c5887b5f04cd0
Anastasiia Nedbailo <nastia.nana@gmail.com>
2020-09-06 17:14:04 +0300

    added releaseDate

--- src/app/services/json-to-firestore.js
+++ src/app/services/json-to-firestore.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-   // TODO-6
+    // TODO-6
 };
 const albums = require('./albums.json');
 const firebase = require('firebase');
@@ -10,16 +10,19 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 albums.forEach(function (obj) {
-    db.collection("albums").add({
-        name: obj.name,
-        band: obj.band,
-        genre: obj.genre,
-        label: obj.label,
-        producer: obj.producer
-    }).then(function (docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function (error) {
-        console.error("Error adding document: ", error);
-    });
+    db.collection("albums")
+        .add({
+            name: obj.name,
+            band: obj.band,
+            genre: obj.genre,
+            label: obj.label,
+            producer: obj.producer,
+            releaseDate: new Date(obj.releaseDate)
+        })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
 });
