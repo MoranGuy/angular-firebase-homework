e6dca2e177774cdd90f7f712e1c8afd2b5ac47c4
Anastasiia Nedbailo <nastia.nana@gmail.com>
2020-09-06 10:51:19 +0300

    added json-to-firestore

---
+++ src/app/services/json-to-firestore.js
@@ -0,0 +1,25 @@
+const firebaseConfig = {
+   // TODO-6
+};
+const albums = require('./albums.json');
+const firebase = require('firebase');
+
+require('firebase/firestore');
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+albums.forEach(function (obj) {
+    db.collection("albums").add({
+        name: obj.name,
+        band: obj.band,
+        genre: obj.genre,
+        label: obj.label,
+        producer: obj.producer
+    }).then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+        console.error("Error adding document: ", error);
+    });
+});
